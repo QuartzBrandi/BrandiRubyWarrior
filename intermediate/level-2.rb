a12e4e155968b67c49c345d86d6b9809024e52de
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-06-12 12:29:42 -0700

    He attacks! WOOT WOOT!

--- intermediate/level-2.rb
+++ intermediate/level-2.rb
@@ -7,7 +7,9 @@ class Player
     @warrior = warrior_obj
     @current_direction = :forward
 
-    warrior.send(action, where_stairs)
+    # warrior.send(action, where_stairs)
+    action
+
   end
 
   def space(direction)
@@ -19,50 +21,38 @@ class Player
     warrior.direction_of_stairs
   end
 
+  # def action
+  #   hero_action = :walk!, direction
+  #   hero_action = :attack! if space(where_stairs).enemy?
+  #
+  #   return hero_action
+  # end
+
   def action
-    hero_action = :walk!
-    hero_action = :attack! if space(where_stairs).enemy?
-
-    return hero_action
+    if is_x_nearby?(:enemy?)
+      warrior.attack!(direction_of_x(:enemy?))
+    end
   end
 
-  def x_nearby?(search_symbol)
-    if space(:forward).send(search_symbol)
-      direction(:forward)
-      return true
-    elsif space(:backward).send(search_symbol)
-      direction(:backward)
-      return true
-    elsif space(:left).send(search_symbol)
-      direction(:left)
-      return true
-    elsif space(:right).send(search_symbol)
-      direction(:right)
-      return true
-    else
-      return false
-    end
-
+  def is_x_nearby?(search_for) # NEEDS TO BE A SYMBOL AND A SPACE METHOD (empty? enemy? captive? etc.)
     occupied = false
-
     DIRECTIONS.each do |direction|
-      occupied = true if space(direction).send(search_symbol)
+      occupied = true if space(direction).send(search_for)
+    end
+    return occupied
+  end
+
+  def direction_of_x(search_for) # RETURNS FIRST OCCURING DIRECTION OF OBJECT SEARCHING FOR
+    DIRECTIONS.each do |direction|
+      return direction if object_in_direction?(direction, search_for)
     end
   end
 
-  def object_nearby?(object_symbol)
-
+  def object_in_direction?(direction, search_for)
+    space(direction).send(search_for)
   end
 
-  def object_in_direction?(direction, object_symbol)
-    space(direction).send(object_symbol)
-  end
-
-  def x_backward?(search_symbol)
-    space(:backward).send(search_symbol)
-  end
-
-  def change_direction(direct)
-    current_direction = direct
+  def change_direction(direction)
+    current_direction = direction
   end
 end
